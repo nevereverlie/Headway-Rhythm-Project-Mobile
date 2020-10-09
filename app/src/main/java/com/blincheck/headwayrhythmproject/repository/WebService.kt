5a6cd67cabac2ffd4fb0198f38994fea7a9a3cba
Viktor Pinkin <viktor.pinkin@nixsolutions.com>
2020-10-09 23:07:57 +0300

    Enabled api calls for tracks

--- app/src/main/java/com/blincheck/headwayrhythmproject/repository/WebService.kt
+++ app/src/main/java/com/blincheck/headwayrhythmproject/repository/WebService.kt
@@ -2,7 +2,6 @@ package com.blincheck.headwayrhythmproject.repository
 
 import com.blincheck.headwayrhythmproject.enity.Track
 import io.reactivex.Single
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,22 +9,21 @@ import retrofit2.http.GET
 
 interface WebService {
 
-    @GET("/Tracks")
+    @GET("Tracks")
     fun getAllTracks(): Single<List<Track>>
 
     companion object {
 
-        private const val BASE_URL = "https://localhost:5001/api/"
+        private const val BASE_URL = "https://hrp-api.herokuapp.com/api/"
 
         fun getWebService(): WebService {
-            val httpClient = OkHttpClient.Builder().build()
-            return Retrofit.Builder()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(BASE_URL)
-                .client(httpClient)
                 .build()
-                .create(WebService::class.java)
+
+            return retrofit.create(WebService::class.java)
         }
     }
 }
